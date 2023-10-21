ea53f7e45b314406d56007047138d5da481d0396
tcode <tk@tcode.dev>
2023-10-21 20:09:14 +0900

    sample page

---
+++ lib/view/second_page_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SecondPageView extends StatelessWidget {
+  const SecondPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
