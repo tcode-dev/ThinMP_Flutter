3ddf5c440c64f93be3ca18648d2394ca7ea2ba9c
tcode <tk@tcode.dev>
2023-12-30 22:03:08 +0900

    MethodChannel for flutter

--- lib/plugin/songs_plugin.dart
+++ lib/plugin/songs_plugin.dart
@@ -1,1 +1,7 @@
 import 'package:flutter/services.dart';
+
+Future<String> getTest() async {
+  const MethodChannel channel = MethodChannel("Channel");
+  String? resultText = await channel.invokeMethod("test");
+  return resultText ?? "null";
+}
