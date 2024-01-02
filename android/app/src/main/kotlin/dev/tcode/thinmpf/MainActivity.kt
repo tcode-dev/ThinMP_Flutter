5b15575e5fe9ece1499615e9ad1d7668b0931d1a
tcode <tk@tcode.dev>
2024-01-02 23:23:26 +0900

    MethodChannel for android

--- android/app/src/main/kotlin/dev/tcode/thinmpf/MainActivity.kt
+++ android/app/src/main/kotlin/dev/tcode/thinmpf/MainActivity.kt
@@ -5,14 +5,22 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
     private val CHANNEL = "dev.tcode.thinmpf/test"
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-    super.configureFlutterEngine(flutterEngine)
-    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-      call, result ->
-      result.success("test")
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "getTest" ->
+                    result.success("Hello from Kotlin!")
+                else ->
+                    result.success("")
+            }
+            result.success("test")
+        }
     }
-  }
 }
