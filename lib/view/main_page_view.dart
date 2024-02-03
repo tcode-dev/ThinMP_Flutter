ab93ddc487df3fec5a544e1c85642d5abd3b7d5d
tcode <tk@tcode.dev>
2024-02-03 18:27:56 +0900

    Update main_page_view.dart

--- lib/view/main_page_view.dart
+++ lib/view/main_page_view.dart
@@ -2,36 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:thinmpf/view/songs_page_view.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// class MainPageView extends StatefulWidget {
-//   const MainPageView({super.key});
-
-//   @override
-//   State<MainPageView> createState() => _MainPageViewState();
-// }
-// final counterUpdate = (state: int) {
-//   return 1;
-// };
 class MainPageView extends ConsumerWidget {
   const MainPageView({super.key});
-  // String _test = 'test';
-
-  // void _getTest() async {
-  //   String test = await getTest();
-
-  //   setState(() {
-  //     _test = test;
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // final count = ref.watch(counterProvider);
-    // final songs = ref.watch(songsProvider).value;
-    // final test = testAsync.when(
-    //   loading: () => 'loading',
-    //   data: (data) => data,
-    //   error: (error, stacktrace) => 'error',
-    // );
     return Scaffold(
       body: Center(
         child: ElevatedButton(
