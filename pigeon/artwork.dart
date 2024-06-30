e90f27fc018c33d7b1c4345308a51e63c4f6bf65
tcode <tk@tcode.dev>
2024-07-01 08:20:23 +0900

    artwork

---
+++ pigeon/artwork.dart
@@ -0,0 +1,16 @@
+import 'package:pigeon/pigeon.dart';
+
+@HostApi()
+@ConfigurePigeon(PigeonOptions(
+  dartOut: 'lib/pigeon_output/artwork.g.dart',
+  dartOptions: DartOptions(),
+  kotlinOut: 'android/app/src/main/kotlin/dev/tcode/thinmpf/pigeon_output/Artwork.g.kt',
+  kotlinOptions: KotlinOptions(errorClassName: 'ArtworkFlutterError'),
+  objcHeaderOut: 'ios/Runner/PigeonOutput/Artwork.h',
+  objcSourceOut: 'ios/Runner/PigeonOutput/Artwork.m',
+  swiftOut: 'ios/Runner/PigeonOutput/Artwork.g.swift',
+  swiftOptions: SwiftOptions(),
+))
+abstract class HostArtworkApi {
+  Uint8List queryArtwork(String id);
+}
