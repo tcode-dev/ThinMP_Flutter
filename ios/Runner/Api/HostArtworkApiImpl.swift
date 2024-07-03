7e02a8283ecb76a2593520acf140809707e48121
tcode <tk@tcode.dev>
2024-07-04 08:38:52 +0900

    iosの画像取得

--- ios/Runner/Api/HostArtworkApiImpl.swift
+++ ios/Runner/Api/HostArtworkApiImpl.swift
@@ -8,7 +8,16 @@
 import Foundation
 
 class HostArtworkApiImpl: HostArtworkApi {
-    func queryArtwork(id: String) throws -> FlutterStandardTypedData {
-        return nil
+    func queryArtwork(id: String) throws -> FlutterStandardTypedData? {
+        let repository = SongRepository()
+        let song = repository.findById(id: id)
+        let image = song?.artwork?.image(at: CGSize(width: 100, height: 100))
+        let data = image?.pngData() as Data?
+
+        if data == nil {
+            return nil
+        }
+
+        return FlutterStandardTypedData(bytes: data!)
     }
 }
