afdce8efa16d6f0084ba72b033487d082b72d5b9
tcode <tk@tcode.dev>
2024-07-13 08:57:03 +0900

    Create HostArtworkApiImpl.kt

---
+++ android/app/src/main/kotlin/dev/tcode/thinmpf/api/HostArtworkApiImpl.kt
@@ -0,0 +1,10 @@
+package dev.tcode.thinmpf.api
+
+import HostArtworkApi
+import android.content.Context
+
+class HostArtworkApiImpl(private val context: Context) : HostArtworkApi {
+    override fun queryArtwork(id: String): ByteArray? {
+        TODO("Not yet implemented")
+    }
+}
