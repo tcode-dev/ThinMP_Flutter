b5dce2b6fb06c712c511ef05bf0b9143f4219093
tcode <tk@tcode.dev>
2024-07-12 21:03:22 +0900

    androidの曲一覧にidを含める

--- android/app/src/main/kotlin/dev/tcode/thinmpf/api/HostSongApiImpl.kt
+++ android/app/src/main/kotlin/dev/tcode/thinmpf/api/HostSongApiImpl.kt
@@ -5,16 +5,13 @@ import Song
 import android.content.Context
 import dev.tcode.thinmpf.repository.SongRepository
 
-class HostSongApiImpl(private val context: Context): HostSongApi {
+class HostSongApiImpl(private val context: Context) : HostSongApi {
     override fun findAll(): List<Song> {
-//        val song1 = Song("title1", "artist1")
-//        val song2 = Song("title2", "artist2")
         val repository = SongRepository(context)
         val result = repository.findAll()
-        val songs = result.map{
-            Song(it.name, it.artistName)
+
+        return result.map {
+            Song(it.id, it.name, it.artistName)
         }
-//        return listOf(song1, song2)
-        return songs
     }
 }
