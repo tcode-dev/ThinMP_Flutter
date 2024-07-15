cb614e9099bd453586a0783498a9d6c06c1fd2a8
tcode <tk@tcode.dev>
2024-07-16 01:48:30 +0900

    viewModelScope.launch

--- android/app/src/main/kotlin/dev/tcode/thinmpf/api/HostArtworkApiImpl.kt
+++ android/app/src/main/kotlin/dev/tcode/thinmpf/api/HostArtworkApiImpl.kt
@@ -1,46 +1,23 @@
 package dev.tcode.thinmpf.api
 
 import HostArtworkApi
-import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dev.tcode.thinmpf.constant.MediaConstant
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import java.io.ByteArrayOutputStream
 
-//class HostArtworkApiImpl(private val context: Context) : HostArtworkApi {
-//    @RequiresApi(Build.VERSION_CODES.P)
-//    override fun queryArtwork(id: String, callback: (Result<ByteArray?>) -> Unit) {
-//        viewModelScope.launch {
-//            val albumArtUri = Uri.parse("${MediaConstant.ALBUM_ART}/${id}")
-//            val source = ImageDecoder.createSource(context.contentResolver, albumArtUri)
-//            val bitmap = ImageDecoder.decodeBitmap(source)
-//            val stream = ByteArrayOutputStream()
-//
-//            bitmap.compress(Bitmap.CompressFormat.PNG, 90, stream)
-//
-//            callback(Result.success(stream.toByteArray()))
-//        }
-//    }
-//}
-
-class HostArtworkApiImpl(private val context: Context) : HostArtworkApi {
+class HostArtworkApiImpl(private val context: Context) : ViewModel(), HostArtworkApi {
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun queryArtwork(id: String, callback: (Result<ByteArray?>) -> Unit) {
-        val artwork = Artwork()
-
-        artwork.queryArtwork(id, context, callback)
-    }
-}
-
-class Artwork {
-    @SuppressLint("NewApi")
-    @OptIn(DelicateCoroutinesApi::class)
-    fun queryArtwork(id: String, context: Context, callback: (Result<ByteArray?>) -> Unit) {
-        GlobalScope.launch {
+        viewModelScope.launch {
             val albumArtUri = Uri.parse("${MediaConstant.ALBUM_ART}/${id}")
             val source = ImageDecoder.createSource(context.contentResolver, albumArtUri)
             val bitmap = ImageDecoder.decodeBitmap(source)
@@ -52,3 +29,28 @@ class Artwork {
         }
     }
 }
+
+//class HostArtworkApiImpl(private val context: Context) : HostArtworkApi {
+//    @RequiresApi(Build.VERSION_CODES.P)
+//    override fun queryArtwork(id: String, callback: (Result<ByteArray?>) -> Unit) {
+//        val artwork = Artwork(context)
+//
+//        artwork.queryArtwork(id, context, callback)
+//    }
+//}
+//
+//class Artwork(override val coroutineContext: Context) : CoroutineScope {
+//    @RequiresApi(Build.VERSION_CODES.P)
+//    fun queryArtwork(id: String, context: Context, callback: (Result<ByteArray?>) -> Unit) {
+//        launch {
+//            val albumArtUri = Uri.parse("${MediaConstant.ALBUM_ART}/${id}")
+//            val source = ImageDecoder.createSource(context.contentResolver, albumArtUri)
+//            val bitmap = ImageDecoder.decodeBitmap(source)
+//            val stream = ByteArrayOutputStream()
+//
+//            bitmap.compress(Bitmap.CompressFormat.PNG, 90, stream)
+//
+//            callback(Result.success(stream.toByteArray()))
+//        }
+//    }
+//}
