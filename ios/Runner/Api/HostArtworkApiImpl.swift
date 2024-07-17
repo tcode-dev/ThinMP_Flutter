db33b322a2411c19869357bccadd8e5f44e0a5cd
tcode <tk@tcode.dev>
2024-07-18 08:04:15 +0900

    iosの画像を非同期で取得

--- ios/Runner/Api/HostArtworkApiImpl.swift
+++ ios/Runner/Api/HostArtworkApiImpl.swift
@@ -8,16 +8,17 @@
 import Foundation
 
 class HostArtworkApiImpl: HostArtworkApi {
-    func queryArtwork(id: String) throws -> FlutterStandardTypedData? {
+    func queryArtwork(id: String, completion: @escaping (Result<FlutterStandardTypedData?, Error>) -> Void) {
         let repository = SongRepository()
         let song = repository.findById(id: id)
         let image = song?.artwork?.image(at: CGSize(width: 100, height: 100))
         let data = image?.pngData() as Data?
 
         if data == nil {
-            return nil
+            completion(.success(nil))
+            return
         }
 
-        return FlutterStandardTypedData(bytes: data!)
+        completion(.success(FlutterStandardTypedData(bytes: data!)))
     }
 }
