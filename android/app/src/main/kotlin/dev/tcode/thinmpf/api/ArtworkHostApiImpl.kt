bcb72745b0fad7a54bd6cfec6302ac4c296d159a
tcode <tk@tcode.dev>
2024-07-27 15:20:46 +0900

    android: ファイルが無いときのエラー対応

--- android/app/src/main/kotlin/dev/tcode/thinmpf/api/ArtworkHostApiImpl.kt
+++ android/app/src/main/kotlin/dev/tcode/thinmpf/api/ArtworkHostApiImpl.kt
@@ -6,23 +6,32 @@ import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.net.Uri
 import dev.tcode.thinmpf.constant.MediaConstant
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.ByteArrayOutputStream
 
 class ArtworkHostApiImpl(private val context: Context) : ArtworkHostApi {
-    @OptIn(DelicateCoroutinesApi::class)
     override fun queryArtwork(id: String, callback: (Result<ByteArray?>) -> Unit) {
-        GlobalScope.launch {
+        CoroutineScope(Dispatchers.IO).launch {
             val albumArtUri = Uri.parse("${MediaConstant.ALBUM_ART}/${id}")
             val source = ImageDecoder.createSource(context.contentResolver, albumArtUri)
-            val bitmap = ImageDecoder.decodeBitmap(source)
-            val stream = ByteArrayOutputStream()
 
-            bitmap.compress(Bitmap.CompressFormat.PNG, 90, stream)
-
-            callback(Result.success(stream.toByteArray()))
+            try {
+                val stream = ByteArrayOutputStream().use { stream ->
+                    val bitmap = ImageDecoder.decodeBitmap(source)
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 90, stream)
+                    stream.toByteArray()
+                }
+                withContext(Dispatchers.Main) {
+                    callback(Result.success(stream))
+                }
+            } catch (_: Exception) {
+                withContext(Dispatchers.Main) {
+                    callback(Result.success(null))
+                }
+            }
         }
     }
 }
