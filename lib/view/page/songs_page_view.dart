476cc9d4108a876cf7bd02da517544602720bd9a
tcode <tk@tcode.dev>
2024-07-27 09:53:02 +0900

    SongsPageViewのリファクタ

--- lib/view/page/songs_page_view.dart
+++ lib/view/page/songs_page_view.dart
@@ -2,37 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:thinmpf/pigeon_output/player.g.dart';
 import 'package:thinmpf/provider/songs_provider.dart';
-import 'package:thinmpf/view/player/mini_player_widget.dart';
 import 'package:thinmpf/view/row/media_row_widget.dart';
 
 class SongsPageView extends ConsumerWidget {
-  const SongsPageView({super.key});
+  final PlayerHostApi player = PlayerHostApi();
+
+  SongsPageView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final songs = ref.watch(songsProvider).value ?? [];
 
     return Scaffold(
-      body: 
+      body: ListView.builder(
+        itemCount: songs.length,
+        itemBuilder: (context, index) {
+          final song = songs[index]!;
 
-          ListView.builder(
-            itemCount: songs.length,
-            itemBuilder: (context, index) {
-              final song = songs[index]!;
-
-              return GestureDetector(
-                onTap: () {
-                  final player = PlayerHostApi();
-
-                  player.startBySongs(index);
-                },
-                child: MediaRowWidget(song: song),
-              );
+          return GestureDetector(
+            onTap: () {
+              player.startBySongs(index);
             },
-          ),
-          // const MiniPlayerWidget(),
-
-      
+            child: MediaRowWidget(song: song),
+          );
+        },
+      ),
     );
   }
 }
