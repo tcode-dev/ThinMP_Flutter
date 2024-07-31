2528843503f7af9fd39759c878fd008383f0bc74
tcode <tk@tcode.dev>
2024-07-31 09:03:40 +0900

    PlayPauseButtonWidgetのConsumerStatefulWidgetをConsumerWidgetに変更

--- lib/view/button/play_pause_button_widget.dart
+++ lib/view/button/play_pause_button_widget.dart
@@ -5,26 +5,20 @@ import 'package:thinmpf/pigeon_output/player.g.dart';
 import 'package:thinmpf/provider/is_playing_provider.dart';
 import 'package:thinmpf/view/button/button_widget.dart';
 
-class PlayPauseButtonWidget extends ConsumerStatefulWidget {
+class PlayPauseButtonWidget extends ConsumerWidget {
   final SizeConstant size;
-
-  const PlayPauseButtonWidget({Key? key, required this.size}) : super(key: key);
-
-  @override
-  MiniPlayerWidgetState createState() => MiniPlayerWidgetState();
-}
-
-class MiniPlayerWidgetState extends ConsumerState<PlayPauseButtonWidget> {
   final PlayerHostApi player = PlayerHostApi();
 
+  PlayPauseButtonWidget({Key? key, required this.size}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final isPlaying = ref.watch(isPlayingProvider);
 
     if (isPlaying) {
-      return ButtonWidget(icon: Icons.pause_rounded, size: widget.size, callback: () => player.pause());
+      return ButtonWidget(icon: Icons.pause_rounded, size: size, callback: () => player.pause());
     } else {
-      return ButtonWidget(icon: Icons.play_arrow_rounded, size: widget.size, callback: () => player.play());
+      return ButtonWidget(icon: Icons.play_arrow_rounded, size: size, callback: () => player.play());
     }
   }
 }
