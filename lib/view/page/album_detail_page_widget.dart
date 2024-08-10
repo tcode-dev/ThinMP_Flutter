e877d3af41b65a6539686386b6905712707eab59
tcode <tk@tcode.dev>
2024-08-10 10:04:50 +0900

    CustomScrollView

--- lib/view/page/album_detail_page_widget.dart
+++ lib/view/page/album_detail_page_widget.dart
@@ -7,10 +7,7 @@ import 'package:thinmpf/view/image/image_widget.dart';
 class AlbumDetailPageWidget extends ConsumerWidget {
   final String id;
 
-  const AlbumDetailPageWidget({
-    super.key,
-    required this.id,
-  });
+  const AlbumDetailPageWidget({super.key, required this.id});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -23,33 +20,73 @@ class AlbumDetailPageWidget extends ConsumerWidget {
     Size screenSize = MediaQuery.sizeOf(context);
 
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: 0.0,
-            right: 0.0,
-            left: 0.0,
-            child: Blur(
-              blur: 10,
-              blurColor: Colors.white,
-              child: ImageWidget(id: album.imageId, size: screenSize.width),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: true,
+            snap: false,
+            floating: false,
+            expandedHeight: screenSize.width - 50,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(album.title),
+              background: Stack(
+                children: [
+                  Positioned(
+                    top: 0.0,
+                    right: 0.0,
+                    left: 0.0,
+                    child: Blur(
+                      blur: 10,
+                      blurColor: Colors.white,
+                      child: ImageWidget(id: album.imageId, size: screenSize.width),
+                    ),
+                  ),
+                  Positioned(
+                    top: screenSize.width - 200,
+                    width: screenSize.width,
+                    child: Container(
+                      height: 200,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                          colors: <Color>[
+                            Theme.of(context).scaffoldBackgroundColor,
+                            Colors.transparent,
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: screenSize.width * 0.15,
+                    left: screenSize.width * 0.15,
+                    child: ImageWidget(id: album.imageId, size: screenSize.width * 0.7),
+                  ),
+                ],
+              ),
             ),
           ),
-          Positioned(
-            top: screenSize.width - 200,
-            width: screenSize.width,
-            child: Container(
-              height: 200,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                  colors: <Color>[
-                    Theme.of(context).scaffoldBackgroundColor,
-                    Colors.transparent,
-                  ],
-                ),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 20,
+              child: Center(
+                child: Text(album.artist),
               ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  color: index.isOdd ? Colors.white : Colors.black12,
+                  height: 100.0,
+                  child: Center(
+                    child: Text('$index', textScaler: const TextScaler.linear(5)),
+                  ),
+                );
+              },
+              childCount: 20,
             ),
           ),
         ],
