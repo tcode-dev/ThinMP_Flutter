936bcefeff79d0014242a026d5465adfb917bb1a
tcode <tk@tcode.dev>
2024-08-11 09:20:11 +0900

    アルバム詳細にミニプレイヤーを追加

--- lib/view/page/album_detail_page_widget.dart
+++ lib/view/page/album_detail_page_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:thinmpf/provider/album_provider.dart';
 import 'package:thinmpf/provider/album_songs_provider.dart';
 import 'package:thinmpf/view/image/image_widget.dart';
+import 'package:thinmpf/view/player/mini_player_widget.dart';
 import 'package:thinmpf/view/row/media_row_widget.dart';
 
 class AlbumDetailPageWidget extends ConsumerWidget {
@@ -22,60 +23,70 @@ class AlbumDetailPageWidget extends ConsumerWidget {
     }
 
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            pinned: true,
-            expandedHeight: screenSize.width - top,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text(album.title),
-              background: Stack(
-                children: [
-                  Positioned(
-                    top: 0.0,
-                    right: 0.0,
-                    left: 0.0,
-                    child: ImageWidget(id: album.imageId, size: screenSize.width),
-                  ),
-                  Positioned(
-                    top: screenSize.width - 200,
-                    width: screenSize.width,
-                    child: Container(
-                      height: 200,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.bottomCenter,
-                          end: Alignment.topCenter,
-                          colors: <Color>[
-                            Theme.of(context).scaffoldBackgroundColor,
-                            Color(0x00ffffff),
-                          ],
+      body: Stack(
+        children: [
+          CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                pinned: true,
+                expandedHeight: screenSize.width - top,
+                flexibleSpace: FlexibleSpaceBar(
+                  title: Text(album.title),
+                  background: Stack(
+                    children: [
+                      Positioned(
+                        top: 0.0,
+                        right: 0.0,
+                        left: 0.0,
+                        child: ImageWidget(id: album.imageId, size: screenSize.width),
+                      ),
+                      Positioned(
+                        top: screenSize.width - 200,
+                        width: screenSize.width,
+                        child: Container(
+                          height: 200,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                              colors: <Color>[
+                                Theme.of(context).scaffoldBackgroundColor,
+                                Color(0x00ffffff),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: 20,
-              child: Center(
-                child: Text(album.artist),
+              SliverToBoxAdapter(
+                child: SizedBox(
+                  height: 20,
+                  child: Center(
+                    child: Text(album.artist),
+                  ),
+                ),
               ),
-            ),
+              SliverFixedExtentList(
+                itemExtent: 60,
+                delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () {
+                      // player.startAllSongs(index);
+                    },
+                    child: MediaRowWidget(song: albumSongs[index]!),
+                  );
+                }, childCount: albumSongs.length),
+              ),
+            ],
           ),
-          SliverFixedExtentList(
-            itemExtent: 60,
-            delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
-              return GestureDetector(
-                onTap: () {
-                  // player.startAllSongs(index);
-                },
-                child: MediaRowWidget(song: albumSongs[index]!),
-              );
-            }, childCount: albumSongs.length),
+          const Positioned(
+            right: 0.0,
+            bottom: 0.0,
+            left: 0.0,
+            child: MiniPlayerWidget(),
           ),
         ],
       ),
