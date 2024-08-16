8f0f12747ab8265c109062e66508eefaafaf223b
tcode <tk@tcode.dev>
2024-08-17 07:33:17 +0900

    openAppSettings

--- lib/view/permission/permission_widget.dart
+++ lib/view/permission/permission_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:thinmpf/constant/permission_constant.dart';
 import 'package:thinmpf/util/platform_select.dart';
+import 'package:thinmpf/view/text/text_widget.dart';
 
 class PermissionWidget extends StatefulWidget {
   final Widget child;
@@ -27,7 +28,25 @@ class _PermissionWidgetState extends State<PermissionWidget> {
         if (snapshot.data!.values.every((status) => status.isGranted)) {
           return widget.child;
         }
-        return Container();
+        return Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(right: 20.0, bottom: 50.0, left: 20.0),
+                  child: TextWidget(text: "Permission has been denied. You can enable the permission in system settings."),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    openAppSettings();
+                  },
+                  child: const Text('open app settings'),
+                ),
+              ],
+            ),
+          ),
+        );
       },
     );
   }
