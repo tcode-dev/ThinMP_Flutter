1497195fdf632c8c7aa32364402ea590af9629b5
tcode <tk@tcode.dev>
2024-08-19 10:23:00 +0900

    add model in flutter

---
+++ lib/model/album_model.dart
@@ -0,0 +1,9 @@
+class AlbumModel {
+  AlbumModel({required this.id, required this.name, required this.artistId, required this.artistName, required this.imageId});
+
+  final String id;
+  final String name;
+  final String artistId;
+  final String artistName;
+  final String imageId;
+}
---
+++ lib/model/song_model.dart
@@ -0,0 +1,13 @@
+class SongModel {
+  SongModel({required this.id, required this.name, required this.albumId, required this.albumName, required this.artistId, required this.artistName, required this.imageId, required this.duration, required this.trackNumber});
+
+  final String id;
+  final String name;
+  final String albumId;
+  final String albumName;
+  final String artistId;
+  final String artistName;
+  final String imageId;
+  final double duration;
+  final double trackNumber;
+}
