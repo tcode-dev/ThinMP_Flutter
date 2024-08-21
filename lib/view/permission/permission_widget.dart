46ac3b5b6a83925118bfbbf0263113aa05c98f03
tcode <tk@tcode.dev>
2024-08-22 07:49:14 +0900

    permissionのlocalization

--- lib/view/permission/permission_widget.dart
+++ lib/view/permission/permission_widget.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:thinmpf/constant/permission_constant.dart';
 import 'package:thinmpf/util/platform_select.dart';
 import 'package:thinmpf/view/loading/loading_widget.dart';
-import 'package:thinmpf/view/text/text_widget.dart';
 
 class PermissionWidget extends StatefulWidget {
   final Widget child;
@@ -36,15 +36,15 @@ class _PermissionWidgetState extends State<PermissionWidget> {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                const Padding(
-                  padding: EdgeInsets.only(right: 20.0, bottom: 50.0, left: 20.0),
-                  child: TextWidget(text: "Permission has been denied. You can enable the permission in system settings."),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20.0, bottom: 50.0, left: 20.0),
+                  child: Text(AppLocalizations.of(context)!.permissionDenied),
                 ),
                 ElevatedButton(
                   onPressed: () {
                     openAppSettings();
                   },
-                  child: const Text('open app settings'),
+                  child: Text(AppLocalizations.of(context)!.openAppSettings),
                 ),
               ],
             ),
