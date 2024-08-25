584a2da693a94b132d4f0b98b8c9a7d1eda53ffb
tcode <tk@tcode.dev>
2024-08-26 08:37:50 +0900

    main_page_widget

--- lib/view/page/main_page_widget.dart
+++ lib/view/page/main_page_widget.dart
@@ -1,58 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:thinmpf/constant/style_constant.dart';
+import 'package:thinmpf/provider/albums_provider.dart';
+import 'package:thinmpf/util/calc_child_aspect_ratio.dart';
+import 'package:thinmpf/util/calc_cross_axis_count.dart';
+import 'package:thinmpf/view/loading/loading_widget.dart';
 import 'package:thinmpf/view/page/albums_page_widget.dart';
 import 'package:thinmpf/view/page/artists_page_widget.dart';
 import 'package:thinmpf/view/page/songs_page_widget.dart';
 import 'package:thinmpf/view/player/mini_player_widget.dart';
+import 'package:thinmpf/view/row/empty_row_widget.dart';
+import 'package:thinmpf/view/row/plain_row_widget.dart';
 
-class MainPageWidget extends StatelessWidget {
+class PageInfo {
+  final String text;
+  final Widget Function() widgetBuilder;
+
+  PageInfo({required this.text, required this.widgetBuilder});
+}
+
+final List<PageInfo> pageList = [
+  PageInfo(text: "Artists", widgetBuilder: () => ArtistsPageWidget()),
+  PageInfo(text: "Albums", widgetBuilder: () => const AlbumsPageWidget()),
+  PageInfo(text: "Songs", widgetBuilder: () => const SongsPageWidget()),
+];
+
+class MainPageWidget extends ConsumerWidget {
   const MainPageWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncValue = ref.watch(albumsProvider);
+    final screenSize = MediaQuery.sizeOf(context);
+    final crossAxisCount = calcCrossAxisCount(screenSize.width);
+    final childAspectRatio = calcChildAspectRatio(screenSize.width, crossAxisCount);
+    final top = MediaQuery.of(context).padding.top;
+
     return Scaffold(
       body: Stack(
-        children: <Widget>[
-          Positioned(
-            top: 100.0,
-            right: 0.0,
-            left: 0.0,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ArtistsPageWidget()),
-                );
-              },
-              child: const Text('Artists'),
-            ),
-          ),
-          Positioned(
-            top: 200.0,
-            right: 0.0,
-            left: 0.0,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const AlbumsPageWidget()),
-                );
-              },
-              child: const Text('Albums'),
-            ),
-          ),
-          Positioned(
-            top: 300.0,
-            right: 0.0,
-            left: 0.0,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SongsPageWidget()),
-                );
-              },
-              child: const Text('Songs'),
-            ),
+        children: [
+          asyncValue.when(
+            loading: () => const LoadingWidget(),
+            error: (Object error, StackTrace stackTrace) {
+              return ErrorWidget(error);
+            },
+            data: (vm) {
+              return CustomScrollView(slivers: <Widget>[
+                SliverToBoxAdapter(
+                  child: Container(
+                    padding: EdgeInsets.only(top: top, left: 20),
+                    child: Text(AppLocalizations.of(context)!.library, style: Theme.of(context).textTheme.headlineLarge),
+                  ),
+                ),
+                SliverFixedExtentList(
+                  itemExtent: 51,
+                  delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => pageList[index].widgetBuilder()),
+                        );
+                      },
+                      child: PlainRowWidget(title: pageList[index].text),
+                    );
+                  }, childCount: pageList.length),
+                ),
+                const SliverToBoxAdapter(
+                  child: EmptyRowWidget(),
+                ),
+              ]);
+            },
           ),
           const Positioned(
             right: 0.0,
