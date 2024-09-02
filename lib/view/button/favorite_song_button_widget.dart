c70e355ad2aa481be17c69068fdd04a3a265860e
tcode <tk@tcode.dev>
2024-09-03 07:41:44 +0900

    favoriteSong

--- lib/view/button/favorite_song_button_widget.dart
+++ lib/view/button/favorite_song_button_widget.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:thinmpf/constant/style_constant.dart';
+import 'package:thinmpf/provider/player/favorite_song_provider.dart';
 import 'package:thinmpf/view/button/button_widget.dart';
 
-class FavoriteSongButtonWidget extends StatelessWidget {
-  const FavoriteSongButtonWidget({Key? key}) : super(key: key);
+class FavoriteSongButtonWidget extends ConsumerStatefulWidget {
+  const FavoriteSongButtonWidget({super.key});
 
-  void _favoriteSong() {}
+  @override
+  FavoriteSongButtonWidgetState createState() => FavoriteSongButtonWidgetState();
+}
+
+class FavoriteSongButtonWidgetState extends ConsumerState<FavoriteSongButtonWidget> {
+  void _favoriteSong() {
+    ref.read(favoriteSongProvider.notifier).toggleFavoriteSong();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ButtonWidget(icon: Icons.favorite_rounded, size: StyleConstant.button.small, imageSize: StyleConstant.image.small, callback: _favoriteSong);
+    final isfavoriteSong = ref.watch(favoriteSongProvider);
+    final opacity = isfavoriteSong ? StyleConstant.opacity.on : StyleConstant.opacity.off;
+
+    return Opacity(opacity: opacity, child: ButtonWidget(icon: Icons.favorite_rounded, size: StyleConstant.button.small, imageSize: StyleConstant.image.small, callback: _favoriteSong));
   }
 }
