8bea14b630be96d15c0e923738891826d84afc18
tcode <tk@tcode.dev>
2024-09-08 08:42:20 +0900

    PlaylistRealmModel

---
+++ lib/model/realm/playlist_realm_model.dart
@@ -0,0 +1,12 @@
+import 'package:realm/realm.dart';
+
+part 'playlist_realm_model.realm.dart';
+
+@RealmModel()
+class _PlaylistRealmModel {
+  @PrimaryKey()
+  late ObjectId id;
+  late String name;
+  late int order;
+  late List<String> songIds;
+}
