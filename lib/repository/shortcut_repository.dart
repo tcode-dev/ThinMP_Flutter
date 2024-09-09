b3654f98e1ed8d243c15a8e8bd34373cb21c4c33
tcode <tk@tcode.dev>
2024-09-09 21:26:17 +0900

    ShortcutRepository

---
+++ lib/repository/shortcut_repository.dart
@@ -0,0 +1,17 @@
+import 'package:realm/realm.dart';
+import 'package:thinmpf/constant/shortcut_item_type.dart';
+import 'package:thinmpf/model/realm/shortcut_realm_model.dart';
+import 'package:thinmpf/repository/base_repository.dart';
+
+class ShortcutRepository extends BaseRepository<ShortcutRealmModel> {
+  @override
+  Realm realm = Realm(Configuration.local([ShortcutRealmModel.schema]));
+
+  void add(String id, ShortcutItemType type) {
+    final model = ShortcutRealmModel(ObjectId(), id, type.index, incrementOrder());
+
+    realm.write(() {
+      realm.add(model);
+    });
+  }
+}
