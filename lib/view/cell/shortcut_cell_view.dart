225f151c18f6c23c9a22219081340d963404035e
tcode <tk@tcode.dev>
2024-09-12 08:06:44 +0900

    shortcut artist

--- lib/view/cell/shortcut_cell_view.dart
+++ lib/view/cell/shortcut_cell_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:thinmpf/constant/shortcut_item_type.dart';
 import 'package:thinmpf/constant/style_constant.dart';
 import 'package:thinmpf/model/shortcut_model.dart';
+import 'package:thinmpf/view/image/circle_image_widget.dart';
 import 'package:thinmpf/view/image/square_image_widget.dart';
 import 'package:thinmpf/view/text/text_widget.dart';
 
@@ -15,14 +16,18 @@ class ShortcutCellWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final secondaryText = shortcut.type == ShortcutItemType.artist ? 'Artist' : shortcut.type == ShortcutItemType.album ? 'Album' : 'Playlist';
+    final secondaryText = shortcut.type == ShortcutItemType.artist
+        ? 'Artist'
+        : shortcut.type == ShortcutItemType.album
+            ? 'Album'
+            : 'Playlist';
 
     return LayoutBuilder(
       builder: (context, constraints) {
         return Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SquareImageWidget(id: shortcut.imageId, size: constraints.maxWidth),
+            shortcut.type == ShortcutItemType.artist ? CircleImageWidget(id: shortcut.imageId, size: constraints.maxWidth) : SquareImageWidget(id: shortcut.imageId, size: constraints.maxWidth),
             SizedBox(
               height: StyleConstant.row.contentBoxHeight,
               child: Center(
