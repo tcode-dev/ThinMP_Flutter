c3a3d5beb256a7e2cc22088a747ed46379cfd50f
tcode <tk@tcode.dev>
2024-09-16 08:56:30 +0900

    replace MediaActionRowWidget in favorite_songs_page_widget

--- lib/view/page/favorite_songs_page_widget.dart
+++ lib/view/page/favorite_songs_page_widget.dart
@@ -5,11 +5,10 @@ import 'package:thinmpf/constant/style_constant.dart';
 import 'package:thinmpf/pigeon_output/audio.g.dart';
 import 'package:thinmpf/provider/page/favorite_songs_provider.dart';
 import 'package:thinmpf/view/loading/loading_widget.dart';
-import 'package:thinmpf/view/menu/song_context_menu.dart';
 import 'package:thinmpf/view/page/favorite_songs_edit_page_widget.dart';
 import 'package:thinmpf/view/player/mini_player_widget.dart';
 import 'package:thinmpf/view/row/empty_row_widget.dart';
-import 'package:thinmpf/view/row/media_row_widget.dart';
+import 'package:thinmpf/view/row/media_action_row_widget.dart';
 
 final PlayerHostApi _player = PlayerHostApi();
 
@@ -23,6 +22,10 @@ class FavoriteSongsPageWidget extends ConsumerStatefulWidget {
 class FavoriteSongsPageWidgetState extends ConsumerState<FavoriteSongsPageWidget> {
   void _reload() {
     ref.read(favoriteSongsProvider.notifier).reload();
+  }
+
+  void _play(int index, List<String?> ids) {
+    _player.start(index, ids);
   }
 
   @override
@@ -39,12 +42,12 @@ class FavoriteSongsPageWidgetState extends ConsumerState<FavoriteSongsPageWidget
           PopupMenuButton(
             onSelected: (item) async {
               if (item == 'edit') {
-              await Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const FavoriteSongsEditPageWidget()),
-              );
+                await Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FavoriteSongsEditPageWidget()),
+                );
 
-              _reload();
+                _reload();
               }
             },
             itemBuilder: (BuildContext context) => [
@@ -69,11 +72,10 @@ class FavoriteSongsPageWidgetState extends ConsumerState<FavoriteSongsPageWidget
                   SliverFixedExtentList(
                     itemExtent: StyleConstant.row.borderBoxHeight,
                     delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
-                      return GestureDetector(
-                        onTap: () {
-                          _player.start(index, vm.songIds);
-                        },
-                        child: SongContextMenuWidget(callback: () => {}, child: MediaRowWidget(song: vm.songs[index])),
+                      return MediaActionRowWidget(
+                        song: vm.songs[index],
+                        onTap: () => _play(index, vm.songIds),
+                        onLongPress: _reload,
                       );
                     }, childCount: vm.songs.length),
                   ),
