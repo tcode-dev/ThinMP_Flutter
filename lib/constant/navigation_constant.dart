071bd30b821f7ac9803990e911da97fa88fa39c1
tcode <tk@tcode.dev>
2024-09-18 07:42:59 +0900

    NavigationListWidget

---
+++ lib/constant/navigation_constant.dart
@@ -0,0 +1,10 @@
+enum NavigationItemConstant { artists, albums, songs, favoriteSong, favoriteArtist, playlists }
+
+const List<NavigationItemConstant> navigationListConstant = [
+  NavigationItemConstant.artists,
+  NavigationItemConstant.albums,
+  NavigationItemConstant.songs,
+  NavigationItemConstant.favoriteArtist,
+  NavigationItemConstant.favoriteSong,
+  NavigationItemConstant.playlists,
+];
--- lib/view/page/main_page_widget.dart
+++ lib/view/page/main_page_widget.dart
@@ -9,39 +9,17 @@ import 'package:thinmpf/util/calc_grid_aspect_ratio.dart';
 import 'package:thinmpf/util/calc_grid_count.dart';
 import 'package:thinmpf/view/cell/album_cell_widget.dart';
 import 'package:thinmpf/view/cell/shortcut_cell_view.dart';
+import 'package:thinmpf/view/list/navigation_list_widget.dart';
 import 'package:thinmpf/view/loading/loading_widget.dart';
 import 'package:thinmpf/view/menu/album_grid_context_menu.dart';
 import 'package:thinmpf/view/menu/artist_grid_context_menu.dart';
 import 'package:thinmpf/view/menu/playlist_grid_context_menu.dart';
 import 'package:thinmpf/view/page/album_detail_page_widget.dart';
-import 'package:thinmpf/view/page/albums_page_widget.dart';
 import 'package:thinmpf/view/page/artist_detail_page_widget.dart';
-import 'package:thinmpf/view/page/artists_page_widget.dart';
-import 'package:thinmpf/view/page/favorite_artists_page_widget.dart';
-import 'package:thinmpf/view/page/favorite_songs_page_widget.dart';
 import 'package:thinmpf/view/page/playlist_detail_page_widget.dart';
-import 'package:thinmpf/view/page/playlists_page_widget.dart';
-import 'package:thinmpf/view/page/songs_page_widget.dart';
 import 'package:thinmpf/view/player/mini_player_widget.dart';
 import 'package:thinmpf/view/row/empty_row_widget.dart';
-import 'package:thinmpf/view/row/list_item_row_widget.dart';
-import 'package:thinmpf/view/row/plain_row_widget.dart';
 
-class PageInfo {
-  final String Function(BuildContext context) text;
-  final Widget Function() widgetBuilder;
-
-  PageInfo({required this.text, required this.widgetBuilder});
-}
-
-final List<PageInfo> pageList = [
-  PageInfo(text: (BuildContext context) => AppLocalizations.of(context)!.artists, widgetBuilder: () => const ArtistsPageWidget()),
-  PageInfo(text: (BuildContext context) => AppLocalizations.of(context)!.albums, widgetBuilder: () => const AlbumsPageWidget()),
-  PageInfo(text: (BuildContext context) => AppLocalizations.of(context)!.songs, widgetBuilder: () => const SongsPageWidget()),
-  PageInfo(text: (BuildContext context) => AppLocalizations.of(context)!.favoriteSong, widgetBuilder: () => const FavoriteSongsPageWidget()),
-  PageInfo(text: (BuildContext context) => AppLocalizations.of(context)!.favoriteArtist, widgetBuilder: () => const FavoriteArtistsPageWidget()),
-  PageInfo(text: (BuildContext context) => AppLocalizations.of(context)!.playlists, widgetBuilder: () => const PlaylistsPageWidget()),
-];
 final shortcutMap = {
   ShortcutItemType.artist: (ShortcutModel shortcut, int index, Function() callback) => ArtistGridContextMenuWidget(
         artistId: shortcut.itemId,
@@ -100,21 +78,7 @@ class MainPageWidgetState extends ConsumerState<MainPageWidget> {
                       child: Text(AppLocalizations.of(context)!.library, style: Theme.of(context).textTheme.headlineLarge),
                     ),
                   ),
-                  SliverFixedExtentList(
-                    itemExtent: StyleConstant.row.borderBoxHeight,
-                    delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
-                      return GestureDetector(
-                        onTap: () async {
-                          await Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => pageList[index].widgetBuilder()),
-                          );
-                          _reload();
-                        },
-                        child: ListItemRowWidget(child: PlainRowWidget(title: pageList[index].text(context))),
-                      );
-                    }, childCount: pageList.length),
-                  ),
+                  NavigationListWidget(callback: _reload),
                   SliverToBoxAdapter(
                     child: Container(
                       padding: EdgeInsets.only(top: StyleConstant.padding.large, left: StyleConstant.padding.large),
