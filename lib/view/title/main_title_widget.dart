5736070e5f7529be948fdac9394b4547abfc8bf9
tcode <tk@tcode.dev>
2024-09-29 09:29:01 +0900

    mainページの編集ボタンを追加

--- lib/view/title/main_title_widget.dart
+++ lib/view/title/main_title_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:thinmpf/constant/style_constant.dart';
 import 'package:thinmpf/view/text/text_widget.dart';
 
@@ -14,7 +15,30 @@ class MainTitleWidget extends StatelessWidget {
     return SliverToBoxAdapter(
       child: Container(
         padding: EdgeInsets.only(top: top, left: StyleConstant.padding.large),
-        child: TextWidget(text: title, style: Theme.of(context).textTheme.headlineLarge),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextWidget(text: title, style: Theme.of(context).textTheme.headlineLarge),
+            PopupMenuButton(
+              onSelected: (item) async {
+                // if (item == 'edit') {
+                //   await Navigator.push(
+                //     context,
+                //     MaterialPageRoute(builder: (context) => PlaylistDetailEditPageWidget(id: widget.id)),
+                //   );
+
+                //   _load();
+                // }
+              },
+              itemBuilder: (BuildContext context) => [
+                PopupMenuItem(
+                  value: 'edit',
+                  child: Text(AppLocalizations.of(context)!.edit),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
