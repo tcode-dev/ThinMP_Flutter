c078cdc70bc60e6bab0f811356954a0d904afbe9
tcode <tk@tcode.dev>
2024-10-03 16:06:22 +0900

    ショートカットの編集

--- lib/view/page/main_page_edit_page_widget.dart
+++ lib/view/page/main_page_edit_page_widget.dart
@@ -7,8 +7,11 @@ import 'package:thinmpf/model/main_menu_model.dart';
 import 'package:thinmpf/provider/config/main_menu_config_factory_provider.dart';
 import 'package:thinmpf/provider/page/main_menu_provider.dart';
 import 'package:thinmpf/provider/page/main_menu_visibility_provider.dart';
+import 'package:thinmpf/provider/page/shortcut_provider.dart';
 import 'package:thinmpf/view/row/checkbox_row_widget.dart';
 import 'package:thinmpf/view/row/list_item_row_widget.dart';
+import 'package:thinmpf/view/row/shortcut_row_widget.dart';
+import 'package:thinmpf/view/title/section_title_widget.dart';
 
 const double _listTileDefaultHeight = 56.0;
 
@@ -32,6 +35,7 @@ class MainPageEditPageWidgetState extends ConsumerState<MainPageEditPageWidget> 
   List<MainMenuModel> _menuList = [];
   bool _shortcutChecked = false;
   bool _recentChecked = false;
+  List<ShortcutRowWidget> _shortcutWidgetList = [];
 
   @override
   void initState() {
@@ -42,14 +46,17 @@ class MainPageEditPageWidgetState extends ConsumerState<MainPageEditPageWidget> 
   Future<void> _load() async {
     await ref.read(mainMenuProvider.notifier).loadEdit();
     await ref.read(mainMenuVisibilityProvider.notifier).load();
+    await ref.read(shortcutProvider.notifier).fetch();
 
     final mainMenu = ref.watch(mainMenuProvider);
     final mainMenuVisibility = ref.watch(mainMenuVisibilityProvider);
+    final shortcuts = ref.watch(shortcutProvider);
 
     setState(() {
       _menuList = List.from(mainMenu);
       _shortcutChecked = mainMenuVisibility[MainMenuConstant.shortcut]!;
       _recentChecked = mainMenuVisibility[MainMenuConstant.recent]!;
+      _shortcutWidgetList = shortcuts.map((model) => ShortcutRowWidget(shortcut: model)).toList();
     });
   }
 
@@ -174,6 +181,46 @@ class MainPageEditPageWidgetState extends ConsumerState<MainPageEditPageWidget> 
                 ),
               ),
             ),
+          ),
+          SectionTitleWidget(title: localizations.shortcut),
+          SliverReorderableList(
+            itemBuilder: (context, index) => ReorderableDelayedDragStartListener(
+              key: Key(_shortcutWidgetList[index].shortcut.id),
+              index: index,
+              child: Material(
+                child: Dismissible(
+                  key: Key(_shortcutWidgetList[index].shortcut.id),
+                  onDismissed: (direction) {
+                    setState(() {
+                      _shortcutWidgetList.removeAt(index);
+                    });
+                  },
+                  child: ListItemRowWidget(
+                    child: ListTile(
+                      minVerticalPadding: 0.0,
+                      contentPadding: EdgeInsets.only(right: StyleConstant.padding.large),
+                      title: Center(
+                        child: _shortcutWidgetList[index],
+                      ),
+                      trailing: const ReorderableDragStartListener(
+                        index: 0,
+                        child: Icon(Icons.drag_handle),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            itemCount: _shortcutWidgetList.length,
+            onReorder: (int oldIndex, int newIndex) {
+              setState(() {
+                if (oldIndex < newIndex) {
+                  newIndex -= 1;
+                }
+                final item = _shortcutWidgetList.removeAt(oldIndex);
+                _shortcutWidgetList.insert(newIndex, item);
+              });
+            },
           ),
         ],
       ),
---
+++ lib/view/row/shortcut_row_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:thinmpf/constant/shortcut_constant.dart';
+import 'package:thinmpf/constant/style_constant.dart';
+import 'package:thinmpf/model/shortcut_model.dart';
+import 'package:thinmpf/view/image/circle_image_widget.dart';
+import 'package:thinmpf/view/image/square_image_widget.dart';
+import 'package:thinmpf/view/text/text_widget.dart';
+
+class ShortcutRowWidget extends StatelessWidget {
+  final ShortcutModel shortcut;
+
+  const ShortcutRowWidget({
+    super.key,
+    required this.shortcut,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: StyleConstant.row.contentBoxHeight,
+      padding: EdgeInsets.only(right: StyleConstant.padding.small),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(right: StyleConstant.padding.small),
+            child: shortcut.type == ShortcutConstant.artist ? CircleImageWidget(id: shortcut.imageId, size: StyleConstant.image.tiny) : SquareImageWidget(id: shortcut.imageId, size: StyleConstant.image.tiny),
+          ),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextWidget(text: shortcut.name),
+                TextWidget(text: shortcut.dedcription, style: Theme.of(context).textTheme.bodySmall),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
