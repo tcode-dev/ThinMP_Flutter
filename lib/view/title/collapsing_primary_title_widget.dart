c1214a1cfa03f1991606a6dd4c0598443fdf6331
tcode <tk@tcode.dev>
2024-10-15 08:09:16 +0900

    CollapsingPrimaryTitleWidget

---
+++ lib/view/title/collapsing_primary_title_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:thinmpf/view/text/text_widget.dart';
+
+class CollapsingPrimaryTitleWidget extends StatelessWidget {
+  final String? title;
+
+  const CollapsingPrimaryTitleWidget({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40.0),
+      child: TextWidget(text: title ?? ''),
+    );
+  }
+}
