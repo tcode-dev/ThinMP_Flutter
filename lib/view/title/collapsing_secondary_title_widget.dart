56bbb7ccf9619e08563209ea6d641f66a9b1a6b2
tcode <tk@tcode.dev>
2024-10-15 08:09:23 +0900

    CollapsingSecondaryTitleWidget

---
+++ lib/view/title/collapsing_secondary_title_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:thinmpf/constant/style_constant.dart';
+import 'package:thinmpf/view/text/text_widget.dart';
+
+class CollapsingSecondaryTitleWidget extends StatelessWidget {
+  final String? title;
+
+  const CollapsingSecondaryTitleWidget({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Container(
+        height: StyleConstant.row.contentBoxHeight,
+        padding: EdgeInsets.symmetric(horizontal: StyleConstant.padding.large),
+        child: Center(
+          child: TextWidget(text: title ?? ''),
+        ),
+      ),
+    );
+  }
+}
