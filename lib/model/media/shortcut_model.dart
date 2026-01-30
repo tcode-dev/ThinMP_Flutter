f2489bf5a8e7d0c21321fee015cd11d782b21abd
tcode <tk@tcode.dev>
2026-01-30 17:37:19 +0900

    Remove unsupported shortcut type handling in ShortcutModel

--- lib/model/media/shortcut_model.dart
+++ lib/model/media/shortcut_model.dart
@@ -24,8 +24,6 @@ class ShortcutModel {
         return localizations.album;
       case ShortcutConstant.playlist:
         return localizations.playlist;
-      default:
-        throw UnsupportedError('Unsupported shortcut type: $type');
-    }
+      }
   }
 }
